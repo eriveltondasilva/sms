8bb15ae09288290b5dba5063af3507ea34caa401
Erivelton Silva <eriveltondasilva13@gmail.com>
2024-05-04 08:54:50 -0300

    configura laravel permission

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -4,36 +4,47 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Inertia\Middleware;
+use App\Models\AcademicYear;
 
 class HandleInertiaRequests extends Middleware
 {
-    /**
-     * The root template that is loaded on the first page visit.
-     *
-     * @var string
-     */
     protected $rootView = 'app';
 
-    /**
-     * Determine the current asset version.
-     */
     public function version(Request $request): string|null
     {
         return parent::version($request);
     }
 
-    /**
-     * Define the props that are shared by default.
-     *
-     * @return array<string, mixed>
-     */
     public function share(Request $request): array
     {
         return [
             ...parent::share($request),
-            'auth' => [
-                'user' => $request->user(),
-            ],
+            'auth' => $this->getAuthUserData($request),
+            'flash' => $this->getFlashData($request),
         ];
+    }
+
+    private function getAuthUserData(Request $request): array
+    {
+        $user =  $request->user();
+
+        if ($user) {
+            $userData = $user->only('id', 'username', 'email', 'avatar_url');
+            $role = $user->roles->first();
+
+            $activeYear = AcademicYear::IsActive()->year;
+
+            return [
+                'user' => $userData + ['role' => $role],
+                'activeYear' => $activeYear,
+            ];
+        }
+
+        return ['user' => null];
+    }
+
+    private function getFlashData(Request $request): array
+    {
+        return $request->session()->only(['message', 'id']);
     }
 }
