c13d4e18f95a32cf9c1415605a302bf13252311a
Erivelton Silva <eriveltondasilva13@gmail.com>
2024-08-05 20:12:07 -0300

    refatora routes

--- routes/web/index.php
+++ routes/web/index.php
@@ -1,24 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{
     DashboardController,
     ProfileController,
     SocialiteController,
     WelcomeController
 };
-use Illuminate\Support\Facades\Route;
 
 // ### WEB ROUTES ###
 Route::get('/', WelcomeController::class)->name('welcome');
 
-
 // TODO: rota para teste, remover depois
 Route::get('/teste', fn () => 'Hello, World!')->name('test');
 
-
 Route::get('/painel', DashboardController::class)
 ->middleware(['auth', 'verified'])->name('dashboard');
-
 
 Route::middleware('auth')->controller(ProfileController::class)
 ->prefix('/profile')->name('profile.')->group(function () {
@@ -28,15 +25,13 @@ Route::middleware('auth')->controller(ProfileController::class)
     Route::delete('/', 'destroy')->name('destroy');
 });
 
-
 Route::controller(SocialiteController::class)
 ->prefix('/socialite')->name('socialite.')->group(function () {
     Route::get('/redirect', 'redirect')->name('redirect');
     Route::get('/callback', 'callback')->name('callback');
 });
 
-// ### OTHER ###
-
+// ### EXTRA ROUTES ###
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 require __DIR__.'/student.php';
