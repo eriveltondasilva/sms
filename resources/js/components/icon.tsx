7b184dbffe9fbac534f3a200ed30dfc7841aaff4
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-02-24 10:26:42 -0300

    wip

--- resources/js/components/icon.tsx
+++ resources/js/components/icon.tsx
@@ -1,17 +1,22 @@
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils'
 
-import type { LucideIcon, LucideProps } from 'lucide-react';
+import type { LucideIcon, LucideProps } from 'lucide-react'
 
 interface IconProps extends LucideProps {
-    iconNode: LucideIcon;
-    label?: string;
+  iconNode: LucideIcon
+  label?: string
 }
 
-export function Icon({ iconNode: IconComponent, className, label, ...props }: IconProps) {
-    return (
-        <>
-            <IconComponent className={cn('size-4', className)} {...props} />
-            {label && <span className='sr-only'>{label}</span>}
-        </>
-    )
+export function Icon({
+  iconNode: IconComponent,
+  className,
+  label,
+  ...props
+}: IconProps) {
+  return (
+    <>
+      <IconComponent className={cn('size-4', className)} {...props} />
+      {label && <span className='sr-only'>{label}</span>}
+    </>
+  )
 }
