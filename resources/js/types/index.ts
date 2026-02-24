bb5adc8ddd54c3eba07eb3012a5f751a3d9f5ac5
Erivelton Silva <eriveltondasilva13@gmail.com>
2026-02-24 10:24:58 -0300

    wip

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,3 +1,12 @@
 export type * from './auth'
 export type * from './navigation'
 export type * from './ui'
+export type * from './pagination'
+
+export interface DataTableColumn<T> {
+    header: string
+    className?: string
+    headerClassName?: string
+    cellClassName?: string
+    renderCell: (row: T) => React.ReactNode
+}
